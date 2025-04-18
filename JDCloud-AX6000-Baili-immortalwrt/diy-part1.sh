6c8ace74dba30f707ff46eda05ab79baed3d0358
yuguod <60930311+yuguod@users.noreply.github.com>
2025-04-18 17:01:41 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/gdy666/luci-app-lucky package/lucky
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter    
